ecdf0a1e4a2878af4255c264fb97544a900cd8e7
gaborfekete85 <fgabiweb@gmail.com>
2017-05-30 14:32:30 +0200

    OAuth server implementation

---
+++ server/oauth-server.js
@@ -0,0 +1,28 @@
+/**
+ * Created by gaben on 2017.05.30..
+ */
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    oauthserver = require('oauth2-server');
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+app.oauth = oauthserver({
+    model: {}, // See below for specification
+    grants: ['password'],
+    debug: true
+});
+
+app.all('/oauth/token', app.oauth.grant());
+
+app.get('/', app.oauth.authorise(), function (req, res) {
+    res.send('Secret area');
+});
+
+app.use(app.oauth.errorHandler());
+
+app.listen(3000);
